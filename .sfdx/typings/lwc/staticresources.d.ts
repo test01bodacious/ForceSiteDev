719e25eed0cd3d3d2eae5b1f42379f328455c32b
Pritam Raj <pritamraj.rits@gmail.com>
2019-05-01 20:15:57 +0530

    FirstDev Commit

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,52 @@
+declare module "@salesforce/resourceUrl/DisabledFilter" {
+    var DisabledFilter: string;
+    export default DisabledFilter;
+}
+declare module "@salesforce/resourceUrl/Filter" {
+    var Filter: string;
+    export default Filter;
+}
+declare module "@salesforce/resourceUrl/FilterApplied" {
+    var FilterApplied: string;
+    export default FilterApplied;
+}
+declare module "@salesforce/resourceUrl/GoogleSignIn" {
+    var GoogleSignIn: string;
+    export default GoogleSignIn;
+}
+declare module "@salesforce/resourceUrl/HomePageBackground" {
+    var HomePageBackground: string;
+    export default HomePageBackground;
+}
+declare module "@salesforce/resourceUrl/JQuery" {
+    var JQuery: string;
+    export default JQuery;
+}
+declare module "@salesforce/resourceUrl/Lightning_Spinner" {
+    var Lightning_Spinner: string;
+    export default Lightning_Spinner;
+}
+declare module "@salesforce/resourceUrl/Notification" {
+    var Notification: string;
+    export default Notification;
+}
+declare module "@salesforce/resourceUrl/OpenRecord" {
+    var OpenRecord: string;
+    export default OpenRecord;
+}
+declare module "@salesforce/resourceUrl/Opps" {
+    var Opps: string;
+    export default Opps;
+}
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
+declare module "@salesforce/resourceUrl/SomethingWentWrong" {
+    var SomethingWentWrong: string;
+    export default SomethingWentWrong;
+}
+declare module "@salesforce/resourceUrl/tableHeadFixer" {
+    var tableHeadFixer: string;
+    export default tableHeadFixer;
+}
